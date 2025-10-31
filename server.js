f8ef47faa448c7dbb0997ec1b6a91bb2f6614eeb
kenanmusali <textme0306@gmail.com>
2025-10-31 15:53:50 +0400

    backend

--- server.js
+++ server.js
@@ -1,14 +1,44 @@
-require('dotenv').config(); 
+// require('dotenv').config(); 
 
+// const express = require('express');
+// const cors = require('cors');
+// const connectDB = require('./config/database');
+// const routes = require('./routes');
+
+// connectDB();
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json({ limit: '10mb' }));
+// app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+// app.use('/uploads', express.static('uploads'));
+// app.use('/api', routes);
+
+// app.get('/health', (req, res) => {
+//   res.status(200).json({ message: 'Server is running' });
+// });
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/database');
 const routes = require('./routes');
 
+const app = express();
+
 connectDB();
 
-const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["https://career.absheronport.az/"],  
+  credentials: true,
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
@@ -16,10 +46,7 @@ app.use('/uploads', express.static('uploads'));
 app.use('/api', routes);
 
 app.get('/health', (req, res) => {
-  res.status(200).json({ message: 'Server is running' });
+  res.status(200).json({ message: 'Server is running ✅' });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
